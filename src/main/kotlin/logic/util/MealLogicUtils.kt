763a485596078392a316010b5478c29577839d41
pola george <86931053+poula22@users.noreply.github.com>
2025-04-16 10:45:12 +0200

    fix count issue in random meals

--- src/main/kotlin/logic/util/MealLogicUtils.kt
+++ src/main/kotlin/logic/util/MealLogicUtils.kt
@@ -4,7 +4,7 @@ import model.Meal
 
 fun List<Meal>.getRandomMeals(condition: (Meal) -> Boolean, count: Int): List<Meal> {
     return this.filter(condition).run {
-        if (size < 10) this
+        if (size < count) this
         else shuffled().slice(0..<count)
     }
 }
