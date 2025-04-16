e6b9a468280d4f385212be21d18cd185f55cd567
Al-Qassim <qassimali624@gmail.com>
2025-04-16 12:39:24 +0300

    - add the date back to dataModel - zero values for nutretions and minutes are null - add an object for special Tags

---
+++ src/main/kotlin/model/SpecialTags.kt
@@ -0,0 +1,20 @@
+package model
+
+object SpecialTags {
+    const val lowSodium = "low-sodium"
+    const val lowProtein = "low-protein"
+    const val lowcCalorie = "low-calorie"
+    const val lowCholesterol = "low-cholesterol"
+    const val lowCarb = "low-carb"
+    const val lowSaturatedFat = "low-saturated-fat"
+    const val veryLowCarbs = "very-low-carbs"
+    const val lowFatBreakFastEggs = "low-fatbreakfast-eggs"
+    const val eggsBreakfast = "eggs-breakfast"
+    const val eggsDairy = "eggs-dairy"
+    const val eggsHealthy = "eggshealthy"
+    const val sweet60MinutesOrLess = "sweet60-minutes-or-less"
+    const val thirtyMinutesOrLess = "30-minutes-or-less"
+    const val fifteenMinutesOrLess = "15-minutes-or-less"
+    const val healthy = "healthy"
+//    const val healthy2 = "healthy-2"
+}
