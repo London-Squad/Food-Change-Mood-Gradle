13f0dd90ce5c74aee877664a7ceff7491ae4471a
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-04-17 12:37:18 +0300

    improve mealGuessGame rules readability

--- src/main/kotlin/presentation/MealGuessGameView.kt
+++ src/main/kotlin/presentation/MealGuessGameView.kt
@@ -47,7 +47,7 @@ class MealGuessGameView(
 
     private fun printRules(){
         println("Rules: ")
-        println("1. A random meal will be presented and you have to guess the time needed to prepare it.")
+        println("1. A random meal will be presented and you have to guess how many minutes are needed to prepare it.")
         println("2. You have 3 attempts only.")
         println("3. If the guess is incorrect, there will be hint for next attempt.\n")
     }
