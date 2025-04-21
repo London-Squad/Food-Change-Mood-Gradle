95d52699c89e51a433c5e2cd6512b1a0c4e4be1d
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-04-21 14:28:34 +0300

    refactor GetHighCalorieMealsUseCase to use MealSuggester class

--- src/main/kotlin/logic/getHighCalorieMeals/GetHighCalorieMealsUseCase.kt
+++ src/main/kotlin/logic/getHighCalorieMeals/GetHighCalorieMealsUseCase.kt
@@ -1,16 +1,24 @@
 package logic.getHighCalorieMeals
 
+import logic.MealSuggester
 import logic.MealsDataSource
 import model.Meal
 
-class GetHighCalorieMealsUseCase(
-    private val mealsDataSource: MealsDataSource
-) {
-    fun getHighCalorieMeals(): List<Meal> =
-        mealsDataSource
-            .getAllMeals()
-            .filter(::isHighCalorieMeal)
+class GetHighCalorieMealsUseCase(mealsDataSource: MealsDataSource) : MealSuggester(mealsDataSource) {
 
     private fun isHighCalorieMeal(meal: Meal): Boolean =
         meal.nutrition.calories?.let { it > 700f } ?: false
+
+
+    fun initSuggestions() {
+        initSuggestedList()
+    }
+
+    fun suggestHighCalorieMeal(): Meal? {
+        return suggestMeal()
+    }
+
+    override fun isValidSuggestion(meal: Meal): Boolean {
+        return isHighCalorieMeal(meal)
+    }
 }
--- src/main/kotlin/presentation/highCalorieMeals/GetHighCalorieMealsView.kt
+++ src/main/kotlin/presentation/highCalorieMeals/GetHighCalorieMealsView.kt
@@ -13,74 +13,49 @@ class GetHighCalorieMealsView(
 ) : BaseView {
 
     override fun start() {
-        val chunkedHighCalorieMeals = getHighCalorieMealsUseCase.getHighCalorieMeals()
-            .chunked(MAX_NUMBER_OF_MEALS_TO_BE_PRINTED_AT_ONCE)
-
-        var userInput: String?
-        var mealsChunkIndex = 0
-        var mealsChunk: List<Meal>
-
-        do {
-            if (chunkedHighCalorieMeals.isEmpty()) {
-                println("No meals with more than 700 calories found.")
-                break
-            }
-
-            mealsChunk = chunkedHighCalorieMeals[mealsChunkIndex]
-
-            printMealsNames(mealsChunk)
-            printOptions(mealsChunkIndex, chunkedHighCalorieMeals.size)
-
-            userInput = userInputReader.getUserInput()
-
-            when (userInput) {
-                "next" -> if (mealsChunkIndex < chunkedHighCalorieMeals.size - 1) mealsChunkIndex++
-                "back" -> if (mealsChunkIndex > 0) mealsChunkIndex--
-                "0" -> break
-                in (1..mealsChunk.size).map { it.toString() } -> {
-                    printMealAndWaitForEnter(mealsChunk[userInput.toInt() - 1])
-                    break
-                }
-
-                else -> println("Invalid input, try again")
-            }
-
-        } while (mealsChunkIndex < chunkedHighCalorieMeals.size && mealsChunkIndex >= 0)
+        getHighCalorieMealsUseCase.initSuggestions()
+        printHeader()
+        printHighCalorieMealSuggestion()
     }
 
-    private fun printOptions(mealsChunkIndex: Int, mealsChunkSize: Int) {
-        println()
-        if ((mealsChunkIndex + 1) < mealsChunkSize) {
-            println("If you want more meals, write 'next'")
+    private fun printHeader() {
+        println("------------------------------------------")
+        println("      High Calorie Meals (>700 kcal)      ")
+        println("------------------------------------------")
+        println("you can like the meal to see full detail, or dislike it to get another meal.")
+    }
+
+    private fun printHighCalorieMealSuggestion() {
+
+        val highCalorieMeal = getHighCalorieMealsUseCase.suggestHighCalorieMeal()
+
+        if (highCalorieMeal == null) {
+            println("\nNo more meals to suggest!")
+            return
         }
-        if (mealsChunkIndex > 0) {
-            println("If you want the previous meals, write 'back'")
+
+        printMealDescription(highCalorieMeal)
+        println("\nDo you like it? (y = like, n = dislike, x = exit): ")
+        when (getValidInputFromUser()) {
+            "y" -> viewUtil.printMeal(highCalorieMeal)
+            "n" -> printHighCalorieMealSuggestion()
+            "x" -> println("Returning to main menu...")
         }
-        println("If you want the details of one of the meals, enter its number")
-        println("If you want to go back to the main menu, enter 0")
-        println()
-        print("your input: ")
     }
 
-    private fun printMealsNames(meals: List<Meal>) {
-        println()
-        println("---------------------------------------------")
-        println("        High Calorie Meals (>700 kcal)       ")
-        println("---------------------------------------------")
-
-        meals.forEachIndexed { mealIndex, meal ->
-            println("${mealIndex + 1}. ${meal.name} (${meal.nutrition.calories} kcal)")
-        }
-        println("---------------------------------------------")
+    private fun printMealDescription(meal: Meal) {
+        println("\nTry this meal: ${meal.name}")
+        viewUtil.printTextWithinWidth("Description: ${meal.description}")
     }
 
-    private fun printMealAndWaitForEnter(meal: Meal) {
-        viewUtil.printMeal(meal)
-        println("press Enter to go back to main menu")
-        userInputReader.getUserInput()
-    }
+    private fun getValidInputFromUser(): String =
+        userInputReader.getValidUserInput(
+            { it in InputOptions },
+            "your choice: ",
+            "invalid input"
+        )
 
     private companion object {
-        const val MAX_NUMBER_OF_MEALS_TO_BE_PRINTED_AT_ONCE = 10
+        val InputOptions = listOf("y", "n", "x")
     }
 }
