72c6a59168a32bc545dbc69c74af03dd0d7351d5
EL-MANCY <abdelrahmanelmancy@gmail.com>
2025-04-25 17:13:05 +0300

    add fake data to verify passing it to the printer

--- src/test/kotlin/presentation/easyMeal/EasyMealViewTest.kt
+++ src/test/kotlin/presentation/easyMeal/EasyMealViewTest.kt
@@ -1,8 +1,10 @@
 package presentation.easyMeal
 
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import logic.easyMealsSuggestion.EasyMealsSuggestionUseCase
+import mealHelperTest.createMeal
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import presentation.utils.UIMealsListPrinter
@@ -12,6 +14,20 @@ class EasyMealViewTest {
     private lateinit var easyMealView: EasyMealView
     private lateinit var easyMealsSuggestionUseCase: EasyMealsSuggestionUseCase
     private lateinit var uiMealsListPrinter: UIMealsListPrinter
+
+    private val easyMeal1 = createMeal(
+        minutes = 20,
+        tags = listOf("easy"),
+        ingredients = listOf("pasta", "tomato", "cheese"),
+        steps = listOf("Step 1", "Step 2", "Step 3"),
+    )
+
+    private val easyMeal2 = createMeal(
+        minutes = 10,
+        tags = listOf("easy"),
+        ingredients = listOf("bread", "cheese", "butter"),
+        steps = listOf("Step 1", "Step 2"),
+    )
 
     @BeforeEach
     fun setUp() {
@@ -34,11 +50,15 @@ class EasyMealViewTest {
     }
 
     @Test
-    fun `start should call printMeals`() {
+    fun `start should pass the meals list from the use case to the printMeals`() {
+        // Given
+        val easyMeals = listOf(easyMeal1, easyMeal2)
+        every { easyMealsSuggestionUseCase.getRandomMeals() } returns easyMeals
+
         // When
         easyMealView.start()
 
         // Then
-        verify { uiMealsListPrinter.printMeals(emptyList(), "Easy Meals") }
+        verify { uiMealsListPrinter.printMeals(easyMeals, "Easy Meals") }
     }
 }
